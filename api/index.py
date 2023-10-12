c63ecde2dc611a6d5b5f9e1939f40d2c8cd45cff
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2023-10-12 17:59:36 +0000

    initial commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import flask
+import requests
+import base64
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/json/<url>")
+def json_req(url):
+    resp= flask.jsonify(requests.get(
+        base64.b64decode(url.encode("utf-8")).decode("utf-8")
+    ).json())
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
